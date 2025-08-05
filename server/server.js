34d6ccd354d69626adc15c4529e5d0493d83bc68
Vaibhav More <vaibhavcmore23@gmail.com>
2025-08-05 22:37:41 +0530

    add api rate limiting

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import courseRouter from "./routes/courseRoute.js";
 import userRouter from "./routes/userRoute.js";
+import { rateLimit } from 'express-rate-limit'
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,12 +14,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, 
+	limit: 50, 
+	standardHeaders: 'draft-8',
+	legacyHeaders: false, 
+	ipv6Subnet: 56,
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
+
 // Connect to database
 try {
   await connectDB();
 } catch (error) {
   console.error('Failed to start server:', error);
-  process.exit(1);
 }
 
 app.use('/api/courses', courseRouter);
